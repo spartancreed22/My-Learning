c2f7813589bd3709c47318d44cbfd3a4b05657e5
Joshua_Ortiz_1004 <joshuaortizgaitan2004@gmail.com>
2025-12-16 12:48:12 -0500

    finalizacion de Modulo de Funciones

--- Desarrollo_Web_full_Stack/TypeScript/Ejercicios/Funciones_Objetos/Tipo_funcion.ts
+++ Desarrollo_Web_full_Stack/TypeScript/Ejercicios/Funciones_Objetos/Tipo_funcion.ts
@@ -4,7 +4,7 @@
 
     //Empezaremos creando unas variables que seran funciones:
 
-    const AgregarNumeros = (a:number, b:number) => {
+    const multiplicarNumeros = (a:number, b:number): number => {
         return a * b;
     }
 
@@ -13,5 +13,48 @@
     const Suma = (a:number, b:number):number => a + b;
 
     //Ahora, definamos un tipo de función para asegurarnos de que nuestras funciones sigan una estructura específica.
+
+    // Creemos otra funcion
+
+    const Saludo = (nombre: string): string =>  `Hola, ${nombre}`;
+    const DatoBooleano = (valor: boolean): boolean => !valor;
+
+
+    // Ahora, definamos un tipo de función
+
+    // let FuncionTipo;
+
+
+    // Corregiremos el eny solamente con decir que la usaremos para funciones tienes que declarar la palabra en Mayuscula la F:
+    // let FuncionTipo: Function;
+
+
+    // Si deseo ser mas especifico por ejemplo que solo acepte e imprima numeros, entonces 
+
+
+    let FuncionTipo: ( firstArg:number, second_Arg:number ) => number ;
+    //Aqui si le estamos especificando que solo vamos a usarla en funciones que retornen numeros y los tipos en los parametros que debe recibir tambien.
+
+    
+    //Si queremos hacerlo en un string entonces usamos esta sintaxis:
+
+        // let FuncionTipo: ( firstArg:string ) => string ; 
+
+
+    //vamos a asignarle las funciones creadas anteriormente a esta nueva variable de tipo any:
+
+    FuncionTipo = multiplicarNumeros;
+    console.log( FuncionTipo(2, 3) ); // 6
+
+    FuncionTipo = Suma;
+    console.log( FuncionTipo(5, 7) ); // 12
+
+    // FuncionTipo = Saludo;
+    // console.log(FuncionTipo("Joshua Ortiz")); // Hola, Joshua Ortiz
+
+    // FuncionTipo = DatoBooleano;
+    // console.log( FuncionTipo(true) ); // false
+
+    // Pero tengamos en cuenta que no es buena practica dejar a nuestra funcion de tipo any por lo que cambiaremos el codigo que teniamos anteriormente para especificar que tupo de funciones vamos a permitir en nuestra variable FuncionTipo.
 
 })();
